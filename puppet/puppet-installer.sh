5556de987ee50032f4f436eeb4b9136960ddfdb7
jiasir <jiasir@icloud.com>
2014-06-26 10:01:36 +0800

    Update

--- puppet/puppet-installer.sh
+++ puppet/puppet-installer.sh
@@ -11,15 +11,15 @@ if [[ $1 == 'master' ]]; then
 	wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
 	sudo dpkg -i puppetlabs-release-precise.deb
 	sudo apt-get update
-	sudo apt-get install puppetmaster-passenger
-	sudo apt-get install puppetmaster
+	sudo apt-get -y install puppetmaster-passenger
+	sudo apt-get -y install puppetmaster
 fi
 
 if [[ $1 == 'agent' ]]; then
 	wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
 	sudo dpkg -i puppetlabs-release-precise.deb
 	sudo apt-get update
-	sudo apt-get install puppet
+	sudo apt-get -y install puppet
 fi
 
 
