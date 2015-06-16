c6db211db0a662a69243fbff0e0e11db2592c7ce
jiasir <jiasir@icloud.com>
2015-06-16 14:57:38 +0800

    Add timeout and fix bugs

--- url-mon/url-mon.py
+++ url-mon/url-mon.py
@@ -5,6 +5,7 @@ __author__ = 'jiasir'
 import sys
 import logging
 import urllib2
+import time
 
 url = sys.argv[1]
 
@@ -13,15 +14,17 @@ logging.basicConfig(filename='url-mon.log', level=logging.DEBUG, format='%(ascti
 
 def get_site_status(url):
     try:
-        urlfile = urllib2.urlopen(url)
+        urlfile = urllib2.urlopen(url, timeout=3)
         status_code = urlfile.code
         logger.info('{code} {link}'.format(code=status_code, link=url))
         if status_code in (200, 302):
             return 'up', urlfile
-    except:
-        pass
+    except Exception as e:
+        logger.error(e)
     return 'down', None
 
 
 if __name__ == '__main__':
-    get_site_status(url)
+    while True:
+        get_site_status(url)
+        time.sleep(15)
