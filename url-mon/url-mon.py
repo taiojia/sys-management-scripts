658670d0ac3ca44df13058266fbb56e5a83ea4b1
jiasir <jiasir@icloud.com>
2015-06-23 18:19:05 +0800

    Add notifycation feature

--- url-mon/url-mon.py
+++ url-mon/url-mon.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 __author__ = 'jiasir'
 
+"""Usage:
+    python url-mon.py url phone[ phone...]
+"""
 
 import sys
 import logging
 import urllib2
 import time
+import socket
 
 url = sys.argv[1]
+p = sys.argv[2:]
 
 logger = logging.getLogger('url-mon')
 logging.basicConfig(filename='url-mon.log', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+hostname = socket.gethostname()
+
+def notify_msg(phone, msg):
+    return urllib2.urlopen('http://10.10.11.241/sms.php?phone={}&content={}'.format(phone, msg), timeout=3)
+
 
 def get_site_status(url):
     try:
@@ -21,6 +32,8 @@ def get_site_status(url):
             return 'up', urlfile
     except Exception as e:
         logger.error(e)
+        [notify_msg(i, '{}%20cant%20access%20{}'.format(hostname, url[7:])) for i in p]
+
     return 'down', None
 
 
